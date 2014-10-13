4a0a844a14c7bcb37959eb4595560e1d0af66acc
Alex Spinks <emperorlexx@hotmail.com>
2014-10-13 04:58:07 +0100

    Socket table background image

--- src/main/java/ttftcuts/physis/client/gui/GuiSocketTable.java
+++ src/main/java/ttftcuts/physis/client/gui/GuiSocketTable.java
@@ -1,12 +1,17 @@
 package ttftcuts.physis.client.gui;
 
+import org.lwjgl.opengl.GL11;
+
 import ttftcuts.physis.Physis;
 import ttftcuts.physis.common.block.tile.TileEntitySocketTable;
 import ttftcuts.physis.common.container.ContainerSocketTable;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.util.ResourceLocation;
 
 public class GuiSocketTable extends GuiContainerPhysis {
 
+	private static final ResourceLocation uiTexture = new ResourceLocation(Physis.MOD_ID, "textures/gui/sockettable.png");
+	
 	public GuiSocketTable(InventoryPlayer inventory, TileEntitySocketTable table) {
 		super(new ContainerSocketTable(inventory, table));
 		this.ySize = 222;
@@ -15,6 +20,15 @@ public class GuiSocketTable extends GuiContainerPhysis {
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float mousex, int mousey, int partialTicks) {
 		ContainerSocketTable cont = (ContainerSocketTable)this.inventorySlots;
+		
+		mc.renderEngine.bindTexture(uiTexture);
+		
+		GL11.glColor4f(1F, 1F, 1F, 1F);
+		GL11.glDisable(GL11.GL_ALPHA_TEST);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		
+		this.drawTexturedModalRect(this.guiLeft - 40, this.guiTop - 65, 0, 0, 256, 256);
 		
 		int sideslots = 5 - cont.activeSlots;
 		
